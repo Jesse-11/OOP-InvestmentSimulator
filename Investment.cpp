0c2fd21bf8d3a2a94dc604c9ed852781fe9e407a
Jesse-11 <jesse.hopppo@gmail.com>
2023-09-16 15:48:43 +0930

    Added comments to code without any

--- Investment.cpp
+++ Investment.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 double Investment::CalculateCurrentPrice() const
 {
-    return quantity * currentPrice;
+    return quantity * currentPrice; // return the current price of the investment
 }
 
 double Investment::CalculateProfitLoss() const
 {
-    return quantity * (currentPrice - purchasePrice);
+    return quantity * (currentPrice - purchasePrice); // return the profit or loss of the investment
 }
 
 void Investment::UpdatePrice(double newPrice)
 {
-    currentPrice = newPrice;
+    currentPrice = newPrice; // update the current price of the investment
 }
--- InvestmentMarket.cpp
+++ InvestmentMarket.cpp
@@ -9,15 +9,16 @@
 
 void InvestmentMarket::AddInvestment(Investment& investment)
 {
-    avaliableInvestments.push_back(investment);
+    avaliableInvestments.push_back(investment); // add new investment to end of the vector
 }
 
 void InvestmentMarket::RemoveInvestment(Investment& investment)
 {
+    // From the begin and end of the vector, use the inline function to compare the Investment and inv to find the investment to be removed
    auto it = remove_if(avaliableInvestments.begin(), avaliableInvestments.end(), [&investment](const Investment& inv) 
    {
-        return inv.GetSymbol() == investment.GetSymbol();
+        return inv.GetSymbol() == investment.GetSymbol(); // return the Symbol of the investment to be removed
    });
-   avaliableInvestments.erase(it, avaliableInvestments.end());
+   avaliableInvestments.erase(it, avaliableInvestments.end()); // erase the investment from the vector 
 }
 
