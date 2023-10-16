09769694b4b1b5ed1d0bf4c57d7ed04e36d876b9
Jesse Hoppo <114092400+Jesse-11@users.noreply.github.com>
2023-10-16 21:58:43 +1030

    Delete InvestmentMarket.cpp

--- InvestmentMarket.cpp
+++
@@ -1,6 +0,0 @@
-#include "InvestmentMarket.h"
-#include "Investment.h"
-
-// No function definitions in this file
-// This is a investment market interface class
-// This file is hence used for compilation purposes only
